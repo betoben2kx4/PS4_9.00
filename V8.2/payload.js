a09b0ccb16d1477cc68ead7cdd84142ff4035ab5
Bishal Hasan Bhuiyan <94838617+bishalqx980@users.noreply.github.com>
2022-12-26 14:06:06 +0600

    Add files via upload

--- V8.2/payload.js
+++ V8.2/payload.js
@@ -2,97 +2,97 @@ function app2usb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "app2usb.bin"
-  loadBIN()
+  poc()
 }
 function appdumper() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "appdumper.bin"
-  loadBIN()
+  poc()
 }
 function dbbackup() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "dbbackup.bin"
-  loadBIN()
+  poc()
 }
 function dbrestore() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "dbrestore.bin"
-  loadBIN()
+  poc()
 }
 function disableaslr() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "disableaslr.bin"
-  loadBIN()
+  poc()
 }
 function dupdate() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "dupdate.bin"
-  loadBIN()
+  poc()
 }
 function eupdate() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "eupdate.bin"
-  loadBIN()
+  poc()
 }
 function exitidu() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "exitidu.bin"
-  loadBIN()
+  poc()
 }
 function ftp() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "ftp.bin"
-  loadBIN()
+  poc()
 }
 function kerneldumper() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "kerneldumper.bin"
-  loadBIN()
+  poc()
 }
 function moduledumper() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "moduledumper.bin"
-  loadBIN()
+  poc()
 }
 function permanentuart() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "permanentuart.bin"
-  loadBIN()
+  poc()
 }
 function ps4debug() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "ps4debug.bin"
-  loadBIN()
+  poc()
 }
 function rifrenamer() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "rifrenamer.bin"
-  loadBIN()
+  poc()
 }
 function todex() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "todex.bin"
-  loadBIN()
+  poc()
 }
 function webrte900() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "webrte900.bin"
-  loadBIN()
+  poc()
 }
 
 // Game_Mods
@@ -100,67 +100,67 @@ function BeefQueefMod100() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "BeefQueefMod100.bin"
-  loadBIN()
+  poc()
 }
 function BeefQueefMod134() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "BeefQueefMod134.bin"
-  loadBIN()
+  poc()
 }
 function BeefQueefMod138() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "BeefQueefMod138.bin"
-  loadBIN()
+  poc()
 }
 function GTAVAG127() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "GTAVAG127.bin"
-  loadBIN()
+  poc()
 }
 function GTAVAG132() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "GTAVAG132.bin"
-  loadBIN()
+  poc()
 }
 function GTAVAG133() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "GTAVAG133.bin"
-  loadBIN()
+  poc()
 }
 function OystersMenuRDR2v138_v100() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "OystersMenuRDR2v138_v100.bin"
-  loadBIN()
+  poc()
 }
 function OystersMenuRDR2v138_v113() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "OystersMenuRDR2v138_v113.bin"
-  loadBIN()
+  poc()
 }
 function OystersMenuRDR2v138_v119() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "OystersMenuRDR2v138_v119.bin"
-  loadBIN()
+  poc()
 }
 function OystersMenuRDR2v138_v124() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "OystersMenuRDR2v138_v124.bin"
-  loadBIN()
+  poc()
 }
 function OystersMenuRDR2v138_v129() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "OystersMenuRDR2v138_v129.bin"
-  loadBIN()
+  poc()
 }
 
 //Linux
@@ -168,30 +168,30 @@ function LinuxLoader1gb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "LinuxLoader1gb.bin"
-  loadBIN()
+  poc()
 }
 function LinuxLoader2gb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "LinuxLoader2gb.bin"
-  loadBIN()
+  poc()
 }
 function LinuxLoader3gb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "LinuxLoader3gb.bin"
-  loadBIN()
+  poc()
 }
 function LinuxLoader4gb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "LinuxLoader4gb.bin"
-  loadBIN()
+  poc()
 }
 function LinuxLoader5gb() {
   ShowMSG = "Please wait...Sending Payload!"
   loadmsg()
   PLfile = "LinuxLoader5gb.bin"
-  loadBIN()
+  poc()
 }
 //@bishalqx980
